05c43076feec761e914a0e3917d5b72b7c9b2bb9
Jeny Sadadia <jeny.sadadia@collabora.com>
2022-06-06 13:03:26 +0530

    kernelci_api.py: add 'get_completed_event'
    Add KernelCI_API.get_completed_event
    to get 'completed' event from API.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

--- kernelci/db/kernelci_api.py
+++ kernelci/db/kernelci_api.py
@@ -103,6 +103,11 @@ class KernelCI_API(Database):
         resp = self._get('/'.join(['get_root_node', node_id]))
         return json.loads(resp.text)
 
+    def get_completed_event(self, node_id):
+        """Get an event when all child nodes are complete of a given node"""
+        resp = self._get('/'.join(['trigger_completed_event', node_id]))
+        return resp.json()
+
     def pubsub_event_filter(self, sub_id, event):
         """Filter Pub/Sub events
 
